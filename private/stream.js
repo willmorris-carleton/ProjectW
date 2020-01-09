92757d5d8f24e643c998a8bfc0f6e1e94300bb93
William Morris <56230234+willmorris-carleton@users.noreply.github.com>
2020-01-09 16:03:08 -0500

    Update stream.js

--- private/stream.js
+++ private/stream.js
@@ -2,13 +2,14 @@
 let socket = io.connect('https://localhost:8080');
 console.log("Socket Created!")
 
-//Send socket string to notify which sound to play
+//Send string through socket to notify which sound to play
 function interact(str) {
     socket.emit('interact', str);
 }
 
-//To recieve stream of images from server
+//To recieve stream of images from server socket
 socket.on('image', (image) => {
+    //Display image to client through HTML
     imageElem = document.getElementById('waitingImg');
     imageElem.src = `data:image/jpeg;base64,${image}`;
 });
